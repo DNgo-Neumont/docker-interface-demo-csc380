6707d71ebfd3b5674eb59ebcdc4fc800020417d2
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2022-01-10 15:19:27 -0700

    Initial commit

---
+++ src/main/java/com/example/dockerinterfacedemo/DockerInterfaceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerinterfacedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerInterfaceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerInterfaceDemoApplication.class, args);
+    }
+
+}
